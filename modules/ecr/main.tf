5358d865f82aba68e349f664eb1156cff5897c91
VarunMayanger <mrmayanger@gmail.com>
2023-08-12 14:31:52 +0530

    fix

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -13,18 +13,4 @@ data "aws_ami" "amazon_linux" {
 
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-0fa167c2af1ea0840"
-  instance_type = "t3.micro"
 
-  subnet_id                   = var.pub_subnet
-  vpc_security_group_ids      = [var.grp_ids]
-  associate_public_ip_address = true
-
-  user_data            = data.template_file.startup.rendered
-  iam_instance_profile = var.iam_instace_profile
-}
-
-data "template_file" "startup" {
-  template = file("boot.sh")
-}
