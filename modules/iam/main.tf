3b7ca68af402712969314356a607aec9786e98ef
VarunMayanger <mrmayanger@gmail.com>
2023-08-12 14:41:52 +0530

    added create user  in iam

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -6,10 +6,14 @@ resource "aws_iam_role" "dev-resources-iam-role" {
   assume_role_policy = <<EOF
 {
 "Version": "2012-10-17",
-"Statement": {
+"Statement": [{
 "Effect": "Allow",
 "Principal": {"Service": "ec2.amazonaws.com"},
 "Action": "sts:AssumeRole"
+},{
+  "Effect": "Allow",
+  "Action": "iam:CreateUser",
+  "Resource": "*"
 }
 }
 EOF
