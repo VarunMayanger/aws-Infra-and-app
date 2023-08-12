3f7d22a62b5da3d8df5c9fb0ef3cc82a9fed405f
VarunMayanger <mrmayanger@gmail.com>
2023-08-12 15:17:14 +0530

    s

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -6,16 +6,12 @@ resource "aws_iam_role" "dev-resources-iam-role" {
   assume_role_policy = <<EOF
 {
 "Version": "2012-10-17",
-"Statement": [{
-  "Effect": "Allow",
-  "Action": "iam:CreateUser",
-  "Resource": "*"
-},
+"Statement": 
 {
 "Effect": "Allow",
 "Principal": {"Service": "ec2.amazonaws.com"},
 "Action": "sts:AssumeRole"
-}]
+}
 }
 EOF
 }
